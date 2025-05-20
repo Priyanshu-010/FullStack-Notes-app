224cb33865411afc141618dc423ce10287d367ce
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-20 17:10:58 +0530

    Routes and Controllers created

---
+++ backend/controller/notes.controller.js
@@ -0,0 +1,12 @@
+export const getNotes = async(req, res) =>{
+  res.send("Notes")
+}
+export const createNote = async(req, res) =>{
+  res.send("Create Notes")
+}
+export const updateNote = async(req, res) =>{
+  res.send("Update Notes")
+}
+export const deleteNote = async(req, res) =>{
+  res.send("Delete Notes")
+}
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import express from "express"
 import dotenv from "dotenv"
+import notesRouter from './route/notes.route.js'
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use('/api/notes', notesRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
