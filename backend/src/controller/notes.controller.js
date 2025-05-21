cf70a36823be5c5f398a6224b5ed9cdde65f4227
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-05-21 22:15:52 +0530

    Crud Controllers Created

--- backend/src/controller/notes.controller.js
+++ backend/src/controller/notes.controller.js
@@ -1,12 +1,52 @@
+import { json } from "express"
+import Note from "../model/Note.model.js"
+
 export const getNotes = async(req, res) =>{
-  res.send("Notes")
+  try {
+    const notes = await Note.find()
+    res.status(200).send(notes)
+  } catch (error) {
+    console.log("Error in getNotes controller", error)
+    res.status(500).send(error)
+  }
 }
 export const createNote = async(req, res) =>{
-  res.send("Create Notes")
+  try {
+    const {title, content} =req.body;
+    const note = new Note({title, content});
+    const savedNote = await note.save();
+    res.status(201).send(savedNote)
+  } catch (error) {
+    console.log("Error in getNotes controller", error)
+    res.status(500).send(error)
+  }
 }
 export const updateNote = async(req, res) =>{
-  res.send("Update Notes")
+  try {
+    const {id} = req.params;
+    const {title, content} =req.body;
+    const note = await Note.findById(id);
+    if(!note){
+      res.status(404).send("Note not found")
+    }
+    const updatedNote = await Note.findByIdAndUpdate(id, {title, content}, {new: true});
+    res.status(200).send(updatedNote)
+  } catch (error) {
+    console.log("Error in getNotes controller", error)
+    res.status(500).send(error)
+  }
 }
 export const deleteNote = async(req, res) =>{
-  res.send("Delete Notes")
+  try {
+    const {id} = req.params;
+    const note = await Note.findById(id);
+    if(!note){
+      res.status(404).send("Note not found")
+    }
+    const deletedNote = await Note.findByIdAndDelete(id);
+    res.status(200).send({message: "Note deleted"})
+  } catch (error) {
+    console.log("Error in getNotes controller", error)
+    res.status(500).send(error)
+  }
 }
