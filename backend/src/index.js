708701ca648f8ddcbd6a73c6a123363970bae7dd
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-06-04 16:22:45 +0530

    Delopyment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,45 @@
-import express from "express"
-import dotenv from "dotenv"
-import notesRouter from './route/notes.route.js'
+import express from "express";
+import dotenv from "dotenv";
+import notesRouter from "./route/notes.route.js";
 import { connectDb } from "./utils/connectdb.js";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
-app.use(cors({
-  origin:"http://localhost:5173"
-}));
+
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url);
+  next();
+});
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-app.use('/api/notes', notesRouter);
+app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDb().then(() =>
   app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
-}));
+  })
+);
